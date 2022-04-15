fe061bd08480a3edab51406414083f723a10c52a
brasslark <eb36912@gmail.com>
2022-04-14 22:12:02 -0700

    ch

--- js/script.js
+++ js/script.js
@@ -17,3 +17,47 @@ window.addEventListener("scroll", () => {
           }
         });
   
+
+        let iframe = document.querySelector('iframe');
+        let player = new Vimeo.Player(iframe);
+        let playing = false;
+        let simulationTime = 0;
+       
+        player.on('play', function(e) {
+            playing = true;
+        });
+       
+        player.on('pause', function(e) {
+            playing = false;
+        });
+       
+        /**
+        * Event fired when user want to fast forward
+        */
+        player.on('seeked', function(e) {
+            if (e.seconds > simulationTime) {
+                player.setCurrentTime(simulationTime).then(function(seconds) {
+                }).catch(function(error) {
+                   switch (error.name) {
+                       case 'RangeError':
+                           // The time is less than 0 or greater than the video's duration
+                           break;
+                       default:
+                           // Some other error occurred
+                           break;
+                   }
+                });
+            }
+            else {
+                simulationTime = data.seconds;
+            }
+        });
+       
+        /**
+        * Keep time going
+        */
+        window.setInterval(function() {
+            if (playing) {
+                simulationTime++;
+            }
+        }, 1000);
